a9dba4598e26b47d24f9083866bead921e4cde16
Chris Fallin <chris@cfallin.org>
2023-10-24 20:15:59 -0700

    WIP.

--- firmware/power.c
+++ firmware/power.c
@@ -7,12 +7,11 @@
 /*
  * PWM output is PB1 (OC1A); active-low (MOSFET is on when output is 0).
  *
- * For a 12V supply and 160V output, we want a duty cycle of ~13.3:1, or (13.3
- * / 14.3) = 93% on-time. Because of the inverting driver, that translates to
- * 7% high PWM. Let's try a comparator value of 7% * 0xffff = 4587.
- *
  * We'll use the ADC in a feedback loop (we want ~1.6V on ADC6) to tune this.
  */
+
+#define PWM_TOP    0x0040
+#define PWM_THRESH 0x0034
 
 void init_power() {
     // Turn on pull-up so the MOSFET is off by default until we configure
@@ -20,8 +19,8 @@ void init_power() {
     // want to fight it.)
     PORTB |= 2;
 
-    ICR1 = 0x0040; // TOP = 0x00ff
-    OCR1A = 0x0034;  // 90% duty cycle
+    ICR1 = PWM_TOP; // TOP = 0x00ff
+    OCR1A = PWM_THRESH;
     TCCR1A = 0x82; // non-inverting PWM mode; WGM11 bit of mode field
     TCCR1B = 0x19; // fast PWM mode (WGM1{3,2,1,0} = 1110), no prescaler
 
@@ -29,9 +28,21 @@ void init_power() {
     DDRB |= 2;
 
     // Turn on ADC.
-    // TODO.
+    ADMUX = 0x66;  // AVcc (5V) reference; ADC6 input.
+    ADCSRB = 0x00; // trigger: free-running mode.
+    // ADEN (enable); ADSC (start conv); ADATE (auto trigger); ADIE (ints);
+    // prescale=128.
+    ADCSRA = 0xef;
 }
 
-void update_power() {
-    // TODO.
+#define VOLTAGE 160
+#define TARGET (uint16_t)((uint32_t)1024 * VOLTAGE / 500)
+
+ISR(ADC_vect) {
+    uint16_t value = (((uint16_t)ADCH) << 8) | ((uint16_t)ADCL);
+    if (value < TARGET) {
+        OCR1A = PWM_THRESH;
+    } else {
+        OCR1A = PWM_TOP;
+    }
 }
